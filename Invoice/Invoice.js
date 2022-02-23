cb654a1daea2360f38e21a7a6404ef6510075bb9
Harsh Panchal <75268993+Harsh5chal@users.noreply.github.com>
2022-02-23 23:34:38 +0530

    Invoice Final

--- Invoice/Invoice.js
+++ Invoice/Invoice.js
@@ -20,8 +20,8 @@ function generatePdf() {
   
       var pdf = new jsPDF('p', 'pt', 'letter');
   
-      var pageHeight = 980;
-      var pageWidth = 900;
+      var pageHeight = 1000;
+      var pageWidth = 1000;
       for (var i = 0; i <= printableArea.clientHeight / pageHeight; i++) {
         var srcImg = canvas;
         var sX = 0;
@@ -39,7 +39,7 @@ function generatePdf() {
         var ctx = onePageCanvas.getContext('2d');
         ctx.drawImage(srcImg, sX, sY, sWidth, sHeight, dX, dY, dWidth, dHeight);
       
-        var canvasDataURL = onePageCanvas.toDataURL("image/png", 1.0);
+        var canvasDataURL = onePageCanvas.toDataURL("logo.png", 1.0);
         var width = onePageCanvas.width;
         var height = onePageCanvas.clientHeight;
       
@@ -54,3 +54,72 @@ function generatePdf() {
     }
   });
 }             
+
+//mul
+function multiplyBy()
+{
+  num1 = document.getElementById("qty").value;
+  num2 = document.getElementById("prc").value;
+  document.getElementById("ttl").innerHTML = num1 * num2;
+  document.getElementById("sub").innerHTML = num1 * num2;
+}
+
+//tax-sub
+function taxBy(){
+  num3 = document.getElementById("sub").value;
+  num4 = document.getElementById("tax").value;
+  document.getElementById("tax-val").innerHTML = (num3 * num4)/100;
+}
+
+//sub-tot
+function subBy()
+{
+  num5 = document.getElementById("sub").value;
+  num6 = document.getElementById("tax-val").value;
+  document.getElementById("sub-tax").innerHTML = parseInt(num5) + parseInt(num6);
+}
+
+//curr-change
+function currChange(){
+  let a = document.getElementById("select").value;
+
+  if(a == "British Pound - GBP"){
+    let p = document.getElementById("prc").value;
+    var m = p*0.0099
+    document.getElementById("prc").value = m;
+  }
+  else if(a == "Indian - INR"){
+    let p = document.getElementById("prc").value;
+    var m = p*1
+    document.getElementById("prc").value = m;
+  }
+  else{
+    let p = document.getElementById("prc").value;
+    var m = p*0.013
+    document.getElementById("prc").value = m;
+  }
+}
+
+//updated amount
+function updateAmount(){
+  num1 = document.getElementById("qty").value;
+  num2 = document.getElementById("prc").value;
+  document.getElementById("ttl").innerHTML = num1 * num2;
+  document.getElementById("sub").innerHTML = num1 * num2;
+}
+
+//tax-sub-update
+function uptaxBy(){
+  num3 = document.getElementById("sub").value;
+  num4 = document.getElementById("tax").value;
+  document.getElementById("tax-val").innerHTML = (num3 * num4)/100;
+}
+
+//updated sub
+function upsubBy()
+{
+  num5 = document.getElementById("sub").value;
+  num6 = document.getElementById("tax-val").value;
+  document.getElementById("sub-tax").innerHTML = parseInt(num5) + parseInt(num6);
+}
+
